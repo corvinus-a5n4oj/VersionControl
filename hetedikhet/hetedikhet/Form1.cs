b04fb1c6ba1636b2ea4d98dcd0fdc33f9cf634ee
Benedek <benedek.nervetti@stud.uni-corvinus.hu>
2021-10-25 19:28:20 +0200

    5,6

--- hetedikhet/hetedikhet/Form1.cs
+++ hetedikhet/hetedikhet/Form1.cs
@@ -9,6 +9,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Xml;
 
 namespace hetedikhet
 {
@@ -18,10 +20,12 @@ namespace hetedikhet
         public Form1()
         {
             InitializeComponent();
-            ValtoArfolyamok();
+            string result2 = ValtoArfolyamok();
+            XmlFeldolgozas(result2);
+            AdatMegjelenites();
             dataGridView1.DataSource = Rates;
         }
-        void ValtoArfolyamok()
+        string ValtoArfolyamok()
         {
 
             var mnbService = new MNBArfolyamServiceSoapClient();
@@ -34,6 +38,51 @@ namespace hetedikhet
             };
             var response = mnbService.GetExchangeRates(request);
             var result = response.GetExchangeRatesResult;
+            return result;
+        }
+        void XmlFeldolgozas(string result) {
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            // Végigmegünk a dokumentum fő elemének gyermekein
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                // Létrehozzuk az adatsort és rögtön hozzáadjuk a listához
+                // Mivel ez egy referencia típusú változó, megtehetjük, hogy előbb adjuk a listához és csak később töltjük fel a tulajdonságait
+                var rate = new RateData();
+                Rates.Add(rate);
+
+                // Dátum
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                // Valuta
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                // Érték
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+            }
+        }
+        void AdatMegjelenites()
+        {
+            chartRateData.DataSource = Rates;
+
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
         }
     }
 }
