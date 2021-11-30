61c0322d15add754f7bdf59cf878717a2a63973e
Benedek <benedek.nervetti@stud.uni-corvinus.hu>
2021-11-30 14:26:53 +0100

    kezdes

--- tizenegyedikhet/tizenegyedikhet/Form1.cs
+++ tizenegyedikhet/tizenegyedikhet/Form1.cs
@@ -16,5 +16,10 @@ namespace tizenegyedikhet
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //dolgok dolgok
+        }
     }
 }
