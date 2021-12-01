9279343618bcd02e6a496ee7afec4f52086585f5
Benedek <benedek.nervetti@stud.uni-corvinus.hu>
2021-12-01 17:00:03 +0100

    Gombra ki

--- tizenegyedikhet/tizenegyedikhet/Form1.cs
+++ tizenegyedikhet/tizenegyedikhet/Form1.cs
@@ -26,22 +26,7 @@ namespace tizenegyedikhet
             BirthProbabilities = GetBirthProbabilities(@"C:\Users\Benedek\Downloads\születés.csv");
             DeathProbabilities = GetDeathProbabilities(@"C:\Users\Benedek\Downloads\halál.csv");
 
-            for (int year = 2005; year <= 2024; year++)
-            {
-                for (int i = 0; i < Population.Count; i++)
-                {
-                    SimStep(2020, Population[i]);
-                }
-
-                int nbrOfMales = (from x in Population
-                                  where x.Gender == Gender.Male && x.IsAlive
-                                  select x).Count();
-                int nbrOfFemales = (from x in Population
-                                    where x.Gender == Gender.Female && x.IsAlive
-                                    select x).Count();
-                Console.WriteLine(
-                    string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));
-            }
+            
         }
         public List<Person> GetPopulation(string csvpath)
         {
@@ -142,6 +127,30 @@ namespace tizenegyedikhet
         private void Form1_Load(object sender, EventArgs e)
         {
             //dolgok dolgok
+        }
+        void Simulation()
+        {
+            for (int year = 2005; year <= 2024; year++)
+            {
+                for (int i = 0; i < Population.Count; i++)
+                {
+                    SimStep(2020, Population[i]);
+                }
+
+                int nbrOfMales = (from x in Population
+                                  where x.Gender == Gender.Male && x.IsAlive
+                                  select x).Count();
+                int nbrOfFemales = (from x in Population
+                                    where x.Gender == Gender.Female && x.IsAlive
+                                    select x).Count();
+                Console.WriteLine(
+                    string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Simulation();
         }
     }
 }
