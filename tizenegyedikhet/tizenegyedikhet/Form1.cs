1ecfcc5b67fd785ee31bec031437a32d114e3ed6
Benedek <benedek.nervetti@stud.uni-corvinus.hu>
2021-12-01 16:58:16 +0100

    Szimulacio

--- tizenegyedikhet/tizenegyedikhet/Form1.cs
+++ tizenegyedikhet/tizenegyedikhet/Form1.cs
@@ -30,7 +30,7 @@ namespace tizenegyedikhet
             {
                 for (int i = 0; i < Population.Count; i++)
                 {
-                    // Ide jön a szimulációs lépés
+                    SimStep(2020, Population[i]);
                 }
 
                 int nbrOfMales = (from x in Population
@@ -62,6 +62,42 @@ namespace tizenegyedikhet
             }
 
             return population;
+        }
+        private void SimStep(int year, Person person)
+        {
+            //Ha halott akkor kihagyjuk, ugrunk a ciklus következő lépésére
+            if (!person.IsAlive) return;
+
+            // Letároljuk az életkort, hogy ne kelljen mindenhol újraszámolni
+            byte age = (byte)(year - person.BirthYear);
+
+            // Halál kezelése
+            // Halálozási valószínűség kikeresése
+            double pDeath = (from x in DeathProbabilities
+                             where x.Age == age
+                             select x.DProbability).FirstOrDefault();
+            //x.Gender == person.Gender &&
+            // Meghal a személy?
+            if (rng.NextDouble() <= pDeath)
+                person.IsAlive = false;
+
+            //Születés kezelése - csak az élő nők szülnek
+            if (person.IsAlive && person.Gender == Gender.Female)
+            {
+                //Szülési valószínűség kikeresése
+                double pBirth = (from x in BirthProbabilities
+                                 where x.Age == age
+                                 select x.BProbability).FirstOrDefault();
+                //Születik gyermek?
+                if (rng.NextDouble() <= pBirth)
+                {
+                    Person újszülött = new Person();
+                    újszülött.BirthYear = year;
+                    újszülött.NbrOfChildren = 0;
+                    újszülött.Gender = (Gender)(rng.Next(1, 3));
+                    Population.Add(újszülött);
+                }
+            }
         }
         public List<BirthProbability> GetBirthProbabilities(string csvpath)
         {
